766ed5ddf00f8729b863327227a71fee8e963586
Francis <francispham89@gmail.com>
2020-03-14 12:29:31 -0700

    Created a Simple Toggle Component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Toggle from './Toggle';
+
 function App() {
   return (
     <div className="App">
@@ -19,6 +21,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Toggle />
     </div>
   );
 }
---
+++ src/Toggle.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+export default class Toggle extends Component {
+    state = {
+        on: false,
+    }
+
+    toggle = () => {
+        this.setState({
+            on: !this.state.on
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.on && (
+                    <h1>Toggle Me</h1>
+                )}
+                <button onClick={this.toggle}>Show/Hide</button>
+            </div>
+        )
+    }
+}
