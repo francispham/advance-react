fd1ff83b49693e7c0666a6bb7f7bde16ae103fe2
Francis <francispham89@gmail.com>
2020-03-14 15:55:14 -0700

    Modal Feature P.2: Optimized Reusable Modal Component

--- src/Modal.js
+++ src/Modal.js
@@ -8,14 +8,15 @@ export default class Modal extends Component {
         const {children, toggle, on } = this.props;
         return (
             <Portal>
-            {on &&
-                <ModalWrapper>
-                    <button onClick={toggle}>Close</button>
-                    <div>
-                        {children}
-                    </div>
-                </ModalWrapper>
-            }
+                {on &&
+                    <ModalWrapper>
+                        <ModalCard>
+                            <CloseButton onClick={toggle}>Close</CloseButton>
+                            <>{children}</>
+                        </ModalCard>
+                        <Background onClick={toggle}/>
+                    </ModalWrapper>
+                }
             </Portal>
         )
     }
@@ -27,5 +28,35 @@ const ModalWrapper = styled.div`
     left:0;
     width: 100%;
     height:100%;
-    background-color: teal;
-`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const ModalCard = styled.div`
+    position: relative;
+    background: white;
+    border-radius: 15px;
+    padding: 15px;
+    min-width: 520px;
+    box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.3);
+    z-index: 10;
+    opacity: 0.95;
+    margin-bottom: 7rem;
+`;
+
+const CloseButton = styled.button`
+    position: absolute;
+    top: 0;
+    right: 0;
+`;
+
+const Background = styled.div`
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    background: black;
+    opacity: 0.5;
+`;
