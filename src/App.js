e2fd5323f49865dae67255237cb0fbcc1863dd6b
Francis <francispham89@gmail.com>
2020-03-15 11:52:14 -0700

    React Context API P1: Setup Context Provider

--- src/App.js
+++ src/App.js
@@ -1,56 +1,78 @@
-import React from 'react';
+import React, { Component, createContext } from 'react';
 import styled from "styled-components";
 import logo from './logo.svg';
 import './App.css';
 
 // Absolute Imports:
 import { Toggle, Portal } from 'Utilities';
-import { Modal } from "Elements";
+import { Modal } from 'Elements';
 
+const UserContext = createContext();
+
+class UserProvider extends Component {
+  state = {
+    id: '1',
+    name: 'Francis',
+    email: 'francispham@gmail.com'
+  }
+  render() {
+    return (
+      <UserContext.Provider
+        value={{
+          user: this.state
+        }}
+      >
+        {this.props.children}
+      </UserContext.Provider>
+    )
+  }
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <br />
-      <Toggle>
-        {({ on, toggle }) => (
-          <>
-            <Button onClick={toggle}>Login</Button>
-            <Modal on={on} toggle={toggle}>
-              <h1>In Modal</h1>
-            </Modal>
-          </>
-        )}
-      </Toggle>
-      <br />
-      <br />
-      <Toggle>
-        {({ on, toggle }) => (
-          <>
-            {" "}
-            {/* This syntax replaces React <Fragment>: cleaner code that not showing <div> tag.*/}
-            {on && <h1>Show Me</h1>}{" "}
-            {/* Similar Syntax: <Component on={on} /> */}
-            <Button onClick={toggle}>Show/Hide</Button>
-            <Portal>{on && <h1>Hi, I am in A Portal</h1>}</Portal>
-          </>
-        )}
-      </Toggle>
-    </div>
+    <UserProvider>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <br />
+        <Toggle>
+          {({ on, toggle }) => (
+            <>
+              <Button onClick={toggle}>Login</Button>
+              <Modal on={on} toggle={toggle}>
+                <h1>In Modal</h1>
+              </Modal>
+            </>
+          )}
+        </Toggle>
+        <br />
+        <br />
+        <Toggle>
+          {({ on, toggle }) => (
+            <>
+              {" "}
+              {/* This syntax replaces React <Fragment>: cleaner code that not showing <div> tag.*/}
+              {on && <h1>Show Me</h1>}{" "}
+              {/* Similar Syntax: <Component on={on} /> */}
+              <Button onClick={toggle}>Show/Hide</Button>
+              <Portal>{on && <h1>Hi, I am in A Portal</h1>}</Portal>
+            </>
+          )}
+        </Toggle>
+      </div>
+    </UserProvider>
   );
 }
 
