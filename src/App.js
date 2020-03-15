fd4ae9477b79f05747d9866abe6db9e1c92291ae
Francis <francispham89@gmail.com>
2020-03-15 12:49:07 -0700

    Code Cleanup

--- src/App.js
+++ src/App.js
@@ -2,42 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import { UserContext } from './UserContext';
+import UserProvider from './UserProvider';
+
 import User from './User';
 
 // Absolute Imports:
 import { Toggle, Portal } from 'Utilities';
 import { Modal, Button } from 'Elements';
-
-
-class UserProvider extends Component {
-  state = {
-    id: '1',
-    name: 'Francis',
-    email: 'francis.pham.ca@gmail.com'
-  }
-
-  logout = () => {
-    this.setState({
-      id: null,
-      name: '',
-      email: ''
-    })
-  }
-
-  render() {
-    return (
-      <UserContext.Provider
-        value={{
-          user: this.state,
-          logout: this.logout
-        }}
-      >
-        {this.props.children}
-      </UserContext.Provider>
-    )
-  }
-};
 
 function App() {
   return (
