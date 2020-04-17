fd38abf386d9238ac36936d6368c1ea3c16aa9df
Francis <francispham89@gmail.com>
2020-04-17 16:02:50 -0700

    Upgraded Full Featured useInc CH

--- src/Components/Inc.js
+++ src/Components/Inc.js
@@ -5,15 +5,21 @@ import { useInc } from '../hooks/useInc';
 import { Button } from 'Elements';
 
 const Inc = () => {
-  const [ value, { inc, dec } ] = useInc();
+  const [volume, { inc, dec, reset }] = useInc({
+    maxValue: 10,
+    minValue: 0,
+    initial: 10,
+  });
 
   return (
     <div>
+      <h3>Volume</h3>
       <Button onClick={dec}>-</Button>
-      {' '}{value}{' '}
-      <Button onClick={inc}>+</Button>
+      {' '}{volume}{' '}
+      <Button onClick={inc}>+</Button>{' '}
+      <Button onClick={reset}>Reset Volume</Button>
     </div>
-  )
-}
+  );
+};
 
 export default Inc;
--- src/hooks/useInc.js
+++ src/hooks/useInc.js
@@ -1,14 +1,26 @@
 import { useState } from 'react'
 
-export const useInc = (initial = 0) => {
+export const useInc = ({
+  maxValue = 100000,
+  minValue = -100000,
+  initial = 0,
+  step = 3,
+}) => {
   const [value, setValue] = useState(initial);
 
   const inc = () => {
-    setValue(prevState => prevState + 1 );
+    setValue(prevState => 
+      prevState + step > maxValue ? maxValue : prevState + step
+    );
   };
 
   const dec = () => {
-    setValue(prevState => prevState - 1 );
+    setValue(prevState =>
+      prevState - step < minValue ? minValue : prevState - step
+    );
   };
-  return [value, { inc, dec }]
+
+  const reset = () => setValue(initial);
+
+  return [value, { inc, dec, reset }];
 }
