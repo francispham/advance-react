fb9b0e40521ef3bbcefe38ee3a37fe2584f72531
Francis <francispham89@gmail.com>
2020-05-12 21:33:03 -0700

    Feature: Implemented Transition with Modal Component

--- src/Components/Elements/Modal.js
+++ src/Components/Elements/Modal.js
@@ -1,26 +1,35 @@
 import React from 'react';
 import styled from 'styled-components';
+import { animated, useTransition } from 'react-spring';
 
 import Icon from './Icon';
 import { Card } from './Cards';
 
-import { Portal } from "../Utilities";
-import { absolute, fixed } from "../Utilities/position";
+import { Portal } from '../Utilities';
+import { absolute, fixed } from '../Utilities/position';
 
 const Modal = ({ toggle, isToggled, children }) => {
-  console.log(this);
+  const transition = useTransition(isToggled, null, {
+    from: { opacity: 0, transform: 'translate3d(0, -40px, 0)' },
+    enter: { opacity: 1, transform: 'translate3d(0, 0, 0)' },
+    leave: { opacity: 0, transform: 'translate3d(0, -40px, 0)' },
+  });
+
   return (
     <Portal>
-      {isToggled && (
-        <ModalWrapper>
-          <ModalCard>
-            <CloseButton onClick={toggle}>
-              <Icon name="close" />
-            </CloseButton>
-            <>{children}</>
-          </ModalCard>
-          <Background onClick={toggle} />
-        </ModalWrapper>
+      {transition.map(
+        ({ item, key, props: animation }) =>
+          item && (
+            <ModalWrapper key={key} style={animation}>
+              <ModalCard animation={animation}>
+                <CloseButton onClick={toggle}>
+                  <Icon name="close" />
+                </CloseButton>
+                <>{children}</>
+              </ModalCard>
+              <Background onClick={toggle} />
+            </ModalWrapper>
+          )
       )}
     </Portal>
   );
@@ -28,7 +37,7 @@ const Modal = ({ toggle, isToggled, children }) => {
 
 export default Modal;
 
-const ModalWrapper = styled.div`
+const ModalWrapper = styled(animated.div)`
   ${fixed({
     y: 'top',
     x: 'left',
